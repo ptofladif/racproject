1df10c03d5cf8306a179515d33dc568e9c674fb1
Marco Fidalgo <marco.fidalgo@mcoutinho.pt>
2021-04-02 05:22:39 +0100

    register confirmation debugging

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@
 
 //Route::group(['prefix' => 'v1', 'as' => 'verification.', 'namespace' => 'Api\V1'], function () {
 //
-//    Route::get('email/verify/{id}', 'VerificationApiController@verify')->name('verify');
-//
-//    Route::get('email/resend', 'VerificationApiController@resend')->name('resend');
+////    Route::get('email/verify/{id}', 'VerificationApiController@verify')->name('verify');
+////
+////    Route::get('email/resend', 'VerificationApiController@resend')->name('resend');
 //
 //});
 
@@ -30,11 +30,6 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::group(['prefix' => 'v1', 'as' => 'client.', 'namespace' => 'Api\V1'], function () {
 
         Route::post('/register', 'AuthApiController@register')->name('register.api');
-
-        Route::get('email/verify/{id}', 'VerificationApiController@verify')->name('verify');
-
-        Route::get('email/resend', 'VerificationApiController@resend')->name('resend');
-
 
         Route::post('/login', 'AuthApiController@login')->name('login.api');
 
