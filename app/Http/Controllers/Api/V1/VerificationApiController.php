dc8331511c6e82889e6d9be592fef52daa548211
Marco Fidalgo <marco.fidalgo@mcoutinho.pt>
2021-04-02 04:53:43 +0100

    register confirmation fix

--- app/Http/Controllers/Api/V1/VerificationApiController.php
+++ app/Http/Controllers/Api/V1/VerificationApiController.php
@@ -1,46 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Foundation\Auth\VerifiesEmails;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class VerificationApiController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Email Verification Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling email verification for any
-    | user that recently registered with the application. Emails may also
-    | be re-sent if the user didn't receive the original email message.
-    |
-    */
-
-    use VerifiesEmails;
-
-    /**
-     * Where to redirect users after verification.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/login';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:6,1')->only('verify', 'resend');
-    }
-
     public function verify($user_id, Request $request) {
         if (!$request->hasValidSignature()) {
             return response()->json(["msg" => "Invalid/Expired url provided."], 401);
@@ -64,4 +30,5 @@ class VerificationApiController extends Controller
 
         return response()->json(["msg" => "Email verification link sent on your email id"]);
     }
+
 }
